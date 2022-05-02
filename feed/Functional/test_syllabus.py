9271280e5e5f2174e97305595e8e6574392e5dad
betty melaku <melakubetty@gmail.com>
2022-05-02 10:58:09 +0300

    func

--- feed/Functional/test_syllabus.py
+++ feed/Functional/test_syllabus.py
@@ -1,29 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import *
-
-
-def init():
-    driverPath = '../../Driver/chromedriver/chromedriver.exe'
-    baseURL = "https://wetechsocial.herokuapp.com/"
-    LoginButton = "//button[contains(text(),'Log into Account')]"
-    MAILInput = "//body/div[@id='root']/div[1]/div[2]/div[1]/div[1]/div[2]/form[1]/input[1]"
-    PasswordInput = "//body/div[@id='root']/div[1]/div[2]/div[1]/div[1]/div[2]/form[1]/input[2]"
-    Mail = "melakubetty@gmail.com"
-    PASSWORD = "123456"
-
-    driver = webdriver.Chrome(driverPath)
-    # Load the feed page
-    driver.get(baseURL)
-    driver.maximize_window()
-    sleep(3)
-    # Login the system with mail and password
-    driver.find_element(By.XPATH, LoginButton).click()
-    driver.find_element(By.XPATH, MAILInput).send_keys(Mail)
-    driver.find_element(By.XPATH, PasswordInput).send_keys(PASSWORD)
-    driver.find_element(By.XPATH, "//button[contains(text(),'Log In')]").click()
-    return driver
-
+from profile import *
 
 def test_syllabus():
     driver = init()
