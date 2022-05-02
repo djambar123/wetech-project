08c9c3c584a32faff94b7c829455bc1c7df84985
betty melaku <melakubetty@gmail.com>
2022-05-02 12:18:32 +0300

    update

--- feed/Functional/syllabus/Syllabus.py
+++ feed/Functional/syllabus/Syllabus.py
@@ -1,21 +1,34 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from time import *
-from profile import *
+from Setup import *
+import unittest
 
-def test_syllabus():
-    driver = init()
-    sleep(3)
-    #
-    driver.find_elements(By.XPATH,"/html[1]/body[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[1]/ul[1]/ul[1]/li")
-    p = 0
-    # clicking buttons
-    while p < 3:
-        html_link = driver.find_elements(By.CLASS_NAME,"links")
-        html_link[p].click()
-        sleep(10)
-        driver.find_element(By.XPATH,"//body[1]/div[2]/div[1]/div[2]").click()
+
+class Syllabus(Setup,unittest.TestCase):
+
+    driver = None
+
+    @classmethod
+    def setUp(self):
+        self.driver = Setup.init(self)
+        return self.driver
+
+    def test_syllabus(self):
         sleep(3)
-        driver.back()
-        sleep(3)
-        p += 1
+        #
+        self.driver.find_elements(By.XPATH,"/html[1]/body[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[1]/ul[1]/ul[1]/li")
+        p = 0
+        # clicking buttons
+        while p < 3:
+            html_link = self.driver.find_elements(By.CLASS_NAME,"links")
+            html_link[p].click()
+            sleep(10)
+            self.driver.find_element(By.XPATH,"//body[1]/div[2]/div[1]/div[2]").click()
+            sleep(3)
+            self.driver.back()
+            sleep(3)
+            p += 1
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
